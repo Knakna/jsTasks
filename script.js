77b9faf57ef529a2702f258e907b3146df47f3b4
knakna <kna@tut.by>
2021-11-03 22:54:33 +0300

    add tasks js  sizes

--- script.js
+++ script.js
@@ -1637,3 +1637,110 @@ console.log(link.tagName);
 //Скрыть/показать элемент
 // link.hidden = true;
 // console.log(link.hidden);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Размеры окна браузера
+// clientWidth и clientHeight
+
+// Доступная ширина и высота окна
+const mainElement = document.documentElement;
+const mainElementWidth = mainElement.clientWidth;
+const mainElementHeight = mainElement.clientHeight;
+
+console.log(mainElementWidth);
+console.log(mainElementHeight);
+
+//-----------
+
+// Ширина и высота окна вместе с полосами прокрутки
+const windowWidth1 = window.innerWidth;
+const windowHeight = window.innerHeight;
+
+console.log(windowWidth1);
+console.log(windowHeight);
+
+
+
+
+// Ширина и высота документа
+// включая прокрученную часть
+
+let scrollWidth = Math.max(
+	document.body.scrollWidth, document.documentElement.scrollWidth,
+	document.body.offsetWidth, document.documentElement.offsetWidth,
+	document.body.clientWidth, document.documentElement.clientWidth
+	);
+	let scrollHeight = Math.max(
+	document.body.scrollHeight, document.documentElement.scrollHeight,
+	document.body.offsetHeight, document.documentElement.offsetHeight,
+	document.body.clientHeight, document.documentElement.clientHeight
+	);
+	console.log(scrollWidth);
+	console.log(scrollHeight);
+
+
+
+	// Получить кол-во прокрученных пикселей
+// Только для чтения
+const windowScrollTop = window.pageYOffset;
+const windowScrollLeft = window.pageXOffset;
+
+console.log(windowScrollTop);
+console.log(windowScrollLeft);
+
+
+
+//---------------------------------
+// Управление прокруткой страницы
+
+
+// Метод scrollBy(x,y) прокручивает страницу относительно
+// её текущего положения.
+
+// function setScrollBy() {
+// window.scrollBy(0, 250);
+// const windowScrollTop = window.pageYOffset;
+// console.log(windowScrollTop);
+// }
+
+
+
+// Метод scrollTo(pageX, pageY) прокручивает страницу
+// на абсолютные координаты(pageX, pageY).
+// тоже самое что и window.scroll()
+
+function setScrollTo() {
+	window.scrollTo(0, 50);
+	const windowScrollTop = window.pageYOffset;
+	console.log(windowScrollTop);
+}
+
+function setScrollToOptions() {
+	window.scrollTo({
+		top: 500,
+		left: 0,
+		// smooth, instant,
+		// либо auto; по умолчанию auto
+		behavior: "smooth"
+	});
+}
+// Опции не работают в Safari
+
+setScrollTo();
+setScrollToOptions();
